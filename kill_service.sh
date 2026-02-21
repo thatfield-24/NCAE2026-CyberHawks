ea8372e451c9ca8dbd5a6fa00f30560e5ab34eec
Tristan Hatfield <tghatfield2005@gmail.com>
2026-02-20 22:12:44 -0600

    Create kill_service.sh
    Kills both the service and socket.
    Goodluck RedTeam service go byebye

---
+++ kill_service.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Used to kill services that are running on the machine, and prevent them from starting again.
+# Usage: sudo ./kill_service.sh <service1> <service2> For example: sudo ./kill_service.sh cron sshd
+for var in "$@"
+do
+	systemctl stop "$var.service" "$var.socket" 
+	systemctl disable "$var.service" "$var.socket"
+	systemctl mask "$var.service" "$var.socket"
+	file=$(systemctl show -P FragmentPath "$var")
+	if [ -n "$file" ]; then
+		mv "$file" "$file.bak"
+		echo "$file" >> kislled_services
+	else
+		echo "$var" >> killed_services
+	fi
+done
+systemctl daemon-reload
