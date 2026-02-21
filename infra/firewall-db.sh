6fb07f6c77022310cb2d5844bedfdae6507372ab
cskutnik <cmskutnik@gmail.com>
2026-02-21 16:15:20 -0600

    Add files via upload
    firewall script for postgres

---
+++ infra/firewall-db.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+iptables -F
+
+# deny outgoing/incoming traffic on default
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+
+# allow loopback
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -i lo -j ACCEPT
+
+# allow established connections
+iptables -A INPUT -m conntrack --cstate ESTABLISHED, RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --cstate ESTABLISHED, RELATED -j ACCEPT
+
+# ping
+iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
+
+# allows scoring user to connect to sql
+iptables -A INPUT -p tcp -s [scoring ip address] --dport 5432 -j ACCEPT
+# iptables -A OUTPUT -p tcp -s [scoring ip address] --dport 5432 -j ACCEPT
+
+# templates in case i need them
+# iptables -A OUTPUT -p [tcp/udp] --dport [port] -j ACCEPT
+
